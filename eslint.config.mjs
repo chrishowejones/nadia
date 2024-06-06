aeb56710252902cce2fa56ad3a66eaf8c2c9b334
Chris Howe-Jones <chrishowe-jones@F124236Y7M.local>
2024-06-06 15:40:49 +0100

    Updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,22 @@
 // eslint.config.js
 import js from "@eslint/js";
+import jest from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
   { ignores: ["public/contrib/*"] },
+  {
+    plugins: {
+      jest,
+    },
+    ...jest.configs["flat/recommended"],
+    files: ["**/*.test.js"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "commonjs",
+      globals: { node: true, jest: true },
+    },
+  },
   {
     files: ["**/*.js"],
     languageOptions: {
@@ -16,15 +29,18 @@ export default [
       "no-multiple-empty-lines": "warn",
       "no-var": "error",
       "prefer-const": "error",
+      ...jest.configs["flat/recommended"].rules,
     },
   },
   {
     files: ["public/**/*.js"],
     languageOptions: {
       sourceType: "script",
-      globals: { browser: true, jquery: true, node: false },
+      globals: { browser: true, jquery: true, node: false, jest: true },
     },
     rules: { "no-var": "off", "prefer-const": "off" },
+    plugins: {
+      jest,
+    },
   },
-  {},
 ];
